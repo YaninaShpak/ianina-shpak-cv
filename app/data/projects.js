746b0bb731c9924f6b518435adfd5e5ba5996b99
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-08-19 17:25:16 +0300

    update projects.js

--- app/data/projects.js
+++ app/data/projects.js
@@ -6,7 +6,7 @@ export const projects = [
     linkProject: "https://yaninashpak.github.io/get_cocktail/",
     linkCode: "https://github.com/YaninaShpak/get_cocktail",
     description: "Сайт для выбора коктейлей по крепости, наличию алкоголя, основному ингредиенту. Есть возможность показать коктейли, в которые не входит какой-либо ингредиент или наоборот - только с выбранным ингредиентом. Также, если не получается определиться с выбором, есть кнопка для рандомного коктейля. Можно отсортировать по алфавиту и крепости, найти коктейль по названию или ингредиентам в составе.",
-    image: "./images/get_cocktail.png"
+    image: "images/get_cocktail.png"
   },
   {
     id: 1,
@@ -15,6 +15,6 @@ export const projects = [
     linkProject: "https://yaninashpak.github.io/daily_balance/",
     linkCode: "https://github.com/YaninaShpak/daily_balance",
     description: "Простое приложение, которое поможет рассчитать Ваш бюджет на день, исходя из Ваших регулярных доходов и расходов. А также поможет определить, сколько оставлять на сбережения и какую сумму Вы сможете накопить за год",
-    image: "./images/daily_balance.png"
+    image: "images/daily_balance.png"
   }
 ];
