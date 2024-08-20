3f6cbcd535091e886a1e0cfe5fb07701082bbc99
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-08-20 12:18:58 +0300

    feat: implement adaptive

--- app/components/Sidebar/Skills/Skills.jsx
+++ app/components/Sidebar/Skills/Skills.jsx
@@ -5,7 +5,7 @@ const Skills = () => {
   return (
     <section className={styles.skills}>
       <h2 className={`title ${styles.title}`}>Навыки</h2>
-      <ul className="skills-list listReset skills__list">
+      <ul className={`${styles.skillsList} listReset skills__list"`}>
         {skillsData.map((el) => (
           <li key={el.title} className={`${styles.item} ${styles[el.icon]}`}>{el.title}</li>
         ))}
