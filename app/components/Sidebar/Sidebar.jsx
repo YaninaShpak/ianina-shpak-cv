08cf893a2497b26ae425c55cba53fabcb7283e5a
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2024-09-30 17:55:43 +0300

    fix: change font

--- app/components/Sidebar/Sidebar.jsx
+++ app/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,15 @@
 import Contacts from "./Contacts/Contacts";
 import styles from "./Sidebar.module.scss";
 import Skills from "./Skills/Skills";
-import { Fuzzy_Bubbles } from "next/font/google";
+import { Nunito } from "next/font/google";
 
-const fuzzyBubbles = Fuzzy_Bubbles({subsets: ["latin"], weight: "400"});
+const nunito = Nunito({ subsets: ["cyrillic"] });
 
 const Sidebar = () => {
   return (
     <aside className={styles.root}>
       <section className={styles.mainInfo}>
-        <h1 className={`${fuzzyBubbles.className} ${styles.mainTitle}`}>Янина Шпак</h1>
+        <h1 className={`${nunito.className} ${styles.mainTitle}`}>Янина Шпак</h1>
         <p className={styles.subtitle}>frontend-developer</p>
       </section>
       <Contacts/>
